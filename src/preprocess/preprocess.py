bd6a678487c1aef8fd05abb928335e0a61d83160
Nora Hallqvist <nhallqvisthellstadius@g.harvard.edu>
2023-09-26 15:13:45 -0400

    preprocess and upload data

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -14,40 +14,16 @@ BUCKET_SRC, BUCKET_DEST = client.bucket(BUCKET_SRC_NAME), client.bucket(
 )
 
 # Get data
-def get_text_data_from_bucket(bucket, object_name):
+def get_text_data_from_bucket(bucket):
     # Access the specific object (text file) in the bucket
-    blob = bucket.blob(object_name)
-    # Download the text data
-    text_data = blob.download_as_text()
-    return text_data
+    blobs = bucket.blob('text/Banana bread')
+    return blobs.download_as_text()
 
-# Example usage
-bucket_name = 'your-bucket-name'
-object_name = 'your-text-file.txt'
-text_data = get_text_data_from_bucket(bucket_name, object_name)
-print(text_data)
-
-# Change data
-
-# ----- upload data to Google buckets -------
-for dish in data_for_dishes:
+def transform_and_upload(bucket, text):
+    transformed_text = text[::-1]
     try:
-        response = requests.get(dish["url_img"])
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            object_name_image = "images/" + dish["name"]
-
-            blob_image = bucket.blob(object_name_image)
-            blob_image.upload_from_string(response.content)
-        else:
-            print("failed to download image")
-
-    except Exception as e:
-        print(f"An error occured with image upload: {str(e)}")
-
-    try:
-        yaml_content = yaml.dump(dish)
-        object_name_text = "text/" + dish["name"]
+        yaml_content = yaml.dump(transformed_text)
+        object_name_text = "texts/" + "banana_bread_test"
         blob_text = bucket.blob(object_name_text)
         blob_text.upload_from_string(yaml_content)
     except Exception as e:
@@ -55,4 +31,6 @@ for dish in data_for_dishes:
 
 
 if __name__ == "__main__":
-    print(get_text_data_from_bucket(BUCKET_SRC, ))
+    text = get_text_data_from_bucket(BUCKET_SRC)
+    transform_and_upload(BUCKET_DEST,text)
+    print("Done")
