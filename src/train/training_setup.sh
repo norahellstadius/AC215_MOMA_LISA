400769ba1c42311f4371feed356434bc3284c484
Anna Midgley <amidgley@g.harvard.edu>
2023-10-06 20:04:37 -0400

    clean up

--- src/train/training_setup.sh
+++ src/train/training_setup.sh
@@ -9,9 +9,6 @@ if [ ! -d "$DESR_DIR" ]; then
   git clone "$RESPOSITORY_URL" "$DESR_DIR"
 fi
 
-# mve train script
-
 cp "train/train_text_to_image.py" "${DESR_DIR}/examples/text_to_image/train_text_to_image.py"
 
 python "train/fetch_train_data.py"
->>>>>>> 78898c7fe71645536578068094815184b13e0fe3
