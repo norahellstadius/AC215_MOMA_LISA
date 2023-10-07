97d35e1bd4ea9deb1317166847b27d9a5a4eacd3
Anna Midgley <amidgley@g.harvard.edu>
2023-10-06 20:09:51 -0400

    add comments

--- src/train/training_setup.sh
+++ src/train/training_setup.sh
@@ -9,6 +9,8 @@ if [ ! -d "$DESR_DIR" ]; then
   git clone "$RESPOSITORY_URL" "$DESR_DIR"
 fi
 
+# copy our training script
 cp "train/train_text_to_image.py" "${DESR_DIR}/examples/text_to_image/train_text_to_image.py"
 
+# run preprocessing
 python "train/fetch_train_data.py"
