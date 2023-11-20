c035314d845b843bc244dc88aebf37be0042732c
Nora Hallqvist <nhallqvisthellstadius@g.harvard.edu>
2023-11-20 09:16:17 -0500

    download button works

--- src/frontend-simple/download.js
+++
@@ -1,8 +0,0 @@
-function download_test() {
-      var a = document.createElement('a');
-      a.href = "images/seb.png";
-      a.download = "NORAÄRBÄST.JPG";
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    };
